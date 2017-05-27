b8f661ab0d83a3a2a147552c8250132858788b11
LucasCorssac <lcorssac@hotmail.com>
2017-05-27 18:13:25 -0300

    added read and write features for database

---
+++ app/src/main/java/com/example/luke/engsoft3/ArtistList.java
@@ -0,0 +1,51 @@
+package com.example.luke.engsoft3;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+/**
+ * Created by Galtama on 27/05/2017.
+ */
+
+public class ArtistList extends ArrayAdapter<Artist>{
+
+    private Activity context;
+    private List<Artist> artistList;
+
+    public ArtistList(Activity context, List<Artist> artistlist){
+        super(context, R.layout.list_layout, artistlist);
+        this.context = context;
+        this.artistList = artistlist;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+
+        View listViewItem = inflater.inflate(R.layout.list_layout, null, true);
+
+        TextView textViewName = (TextView) listViewItem.findViewById(R.id.textViewName);
+        TextView textViewGenre = (TextView) listViewItem.findViewById(R.id.textViewGenre);
+
+        Artist artist = artistList.get(position);
+
+        textViewName.setText(artist.getArtistName());
+        textViewGenre.setText(artist.getArtistGenre());
+
+        return listViewItem;
+
+    }
+}
+
+
